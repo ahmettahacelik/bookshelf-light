db7a5f62c4150b7b629a7ce6950bb3030ca7aa39
ahmettahacelik <108222346+ahmettahacelik@users.noreply.github.com>
2025-07-23 22:37:07 +0300

    Added LoadingEffect
    Some improvements can be made in the future, but it functions correctly

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 
 #include "RainbowEffect.h"
 #include "BreatheEffect.h"
+#include "LoadingEffect.h"
 
 int main(void) {
     stdio_init_all();
+    printf("Starting bookshelf-light...\n");
 
     LEDStrip led_strip;
     led_strip.Update();
@@ -15,18 +17,20 @@ int main(void) {
 
     //RainbowEffect rainbow_effect(&led_strip, 0, 10000, true, true);
 
-    BreatheEffect breathe_effect(&led_strip, 0, 10000, 0x55ff00, 0, 255);
+    //BreatheEffect breathe_effect(&led_strip, 0, 10000, 0x55ff00, 0, 255);
+
+    LoadingEffect loading_effect(&led_strip, 0, 600000, 0x000000, 0x00FFFF);
 
     sleep_ms(1000); // Allow some time for initialization
 
     uint32_t current_time = 0;
     while(true) {
         current_time = to_ms_since_boot(get_absolute_time());
-        if(breathe_effect.Update(current_time)) {
+        if(loading_effect.Update(current_time)) {
             led_strip.Update();
         }
 
-        sleep_ms(20);
+        sleep_ms(1);
     }
 
     return 0;
