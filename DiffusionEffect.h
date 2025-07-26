6aef3e6d41978dd459d4c649b3830e5ca1ffe698
ahmettahacelik <108222346+ahmettahacelik@users.noreply.github.com>
2025-07-26 17:00:10 +0300

    Finalize DiffusionEffect and some to-do's on Effect

--- DiffusionEffect.h
+++ DiffusionEffect.h
@@ -11,13 +11,14 @@ public:
      * @param strip Pointer to the LED strip to apply the effect on
      * @param start_time_ms Start time of the effect in milliseconds
      * @param duration_ms Duration of the effect in milliseconds
-     * @param mutation_rate Rate at which new random colors appear (0.0 - 1.0)
-     * @param min_mutation_interval Minimum interval between mutations in milliseconds
-     * @param diffusion_speed Speed of the diffusion effect (0.0 - 1.0)
+     * @param new_color_rate Rate at which new colors are applied (0.0 to 1.0)
+     * @param new_color_interval Interval for applying new colors in milliseconds
+     * @param diffusion_speed Speed of the diffusion effect (0.0 to 1.0)
+     * @param max_change_per_update Maximum change per update
      */
     DiffusionEffect(LEDStrip* strip, uint32_t start_time_ms, uint32_t duration_ms = 10000,
-        float mutation_rate = 0.1f, uint32_t min_mutation_interval = 10000,
-        float diffusion_speed = 0.5f);
+        float new_color_rate = 0.1f, uint32_t new_color_interval = 1000, float diffusion_speed = 0.2f,
+        int32_t max_change_per_update = 32, uint32_t update_interval = 100);
 
     /**
      * @brief Update the diffusion effect based on the current time
@@ -29,21 +30,28 @@ public:
     bool Update(uint32_t current_time) override;
 
 private:
-    float mutation_rate; ///< Mutation rate for the new random color appears
-    uint32_t last_mutation_time; ///< Last time a mutation occurred
-    uint32_t min_mutation_interval; ///< Minimum interval between mutations in milliseconds
+    uint8_t led_strip_red[NUM_LED]; ///< Array to hold the red component of the LED strip
+    uint8_t led_strip_green[NUM_LED]; ///< Array to hold the green component of the LED strip
+    uint8_t led_strip_blue[NUM_LED]; ///< Array to hold the blue component of
+    float new_color_rate; ///< Rate at which new colors are applied
+    uint32_t new_color_interval; ///< Interval for applying new colors in milliseconds
+    uint32_t last_new_color_time; ///< Last time a new color was applied
+    uint32_t min_diffusion_difference; ///< Minimum difference for diffusion effect
+    int16_t max_change_per_update; ///< Maximum change per update
+    uint32_t last_diffusion_time; ///< Last time diffusion was applied
+    uint32_t update_interval; ///< Interval for updating the diffusion effect
     float diffusion_speed; ///< Speed of diffusion effect
-    uint32_t last_update_time; ///< Last time the effect was updated
+
 
     /**
-     * @brief Calculate the weighted average of three colors for diffusion effect
+     * @brief Calculate the delta component for diffusion effect
      * 
-     * @param left_color left color in RGB format (0xRRGGBB)
-     * @param current_color current color in RGB format (0xRRGGBB)
-     * @param right_color right color in RGB format (0xRRGGBB)
-     * @return uint32_t new color in RGB format (0xRRGGBB)
+     * @param current_color_component Current color component (R, G, or B)
+     * @param right_color_component Right neighbor color component (R, G, or B)
+     * 
+     * @return int16_t Calculated delta component
      */
-    uint32_t WeightedSmoothPixel(uint32_t left_color, uint32_t current_color, uint32_t right_color) const;
+    int16_t CalculateDeltaComponent(uint8_t current_color_component, uint8_t right_color_component) const;
 
 };
 
